c489ca5b261ffb5d91be6bdf656a7688aee71a81
AvisRara <tsiganov.da@gmail.com>
2021-12-23 21:24:48 +0400

    lesson 4.1

--- src/components/app-item-filters/app-item-filters.js
+++ src/components/app-item-filters/app-item-filters.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-const AppItemFilters = () => {
-  return (
-    <div className="btn-group" role="group" aria-label="Basic outlined example">
-      <button type="button" className="btn btn-outline-primary active">
-        All
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Active
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Done
-      </button>
-    </div>
-  );
-};
-
-export default AppItemFilters;
+export default class AppItemFilters extends Component {
+  render() {
+    return (
+      <div
+        className="btn-group"
+        role="group"
+        aria-label="Basic outlined example"
+      >
+        <button type="button" className="btn btn-outline-primary active">
+          All
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Active
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Done
+        </button>
+      </div>
+    );
+  }
+}
--- src/components/app-list-item/app-list-item.js
+++ src/components/app-list-item/app-list-item.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./app-list-item.css";
 
-const AppListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "steelblue" : "black",
-  };
+export default class AppListItem extends Component {
+  
+  render() {
+    const { label, important = false } = this.props;
+    
+    const style = {
+      color: important ? "steelblue" : "black",
+    };
+    
+    return (
+      <span className="app-list-item">
+        <span style={style} className="app-list-item-label">
+          {label}
+        </span>
 
-  return (
-    <span className="app-list-item">
-      <span style={style} className="app-list-item-label">{label}</span>
-      
-      <button type="button" className="btn btn-outline-success float-right">
-        <i className="fas fa-exclamation"></i>
-      </button>
+        <button type="button" className="btn btn-outline-success float-right">
+          <i className="fas fa-exclamation"></i>
+        </button>
 
-      <button type="button" className="btn btn-outline-danger float-right">
-        <i className="far fa-trash-alt"></i>
-      </button>
-    </span>
-  );
-};
+        <button type="button" className="btn btn-outline-danger float-right">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </span>
+    );
+  }
+}
 
-export default AppListItem;
