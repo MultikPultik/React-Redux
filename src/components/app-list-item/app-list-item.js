7ade3301131a1dc8de7cb64b02723df4dbe0220c
AvisRara <tsiganov.da@gmail.com>
2021-12-23 21:53:29 +0400

    lesson 4.2

--- src/components/app-list-item/app-list-item.js
+++ src/components/app-list-item/app-list-item.js
@@ -4,16 +4,33 @@ import "./app-list-item.css";
 
 export default class AppListItem extends Component {
   
+  //Это старый способ вытащить this
+  // constructor() {
+  //   super();
+  //   this.onLabelClick = () => {
+  //     console.log(`${this.props.label}`);
+  //   };
+  // }
+
+  //это новый способ. Заменяет constructor и super
+  onLabelClick = () => {
+      console.log(`${this.props.label}`);
+  };
+
   render() {
     const { label, important = false } = this.props;
-    
+
     const style = {
       color: important ? "steelblue" : "black",
     };
-    
+
     return (
       <span className="app-list-item">
-        <span style={style} className="app-list-item-label">
+        <span
+          style={style}
+          className="app-list-item-label"
+          onClick={this.onLabelClick}
+        >
           {label}
         </span>
 
@@ -28,4 +45,3 @@ export default class AppListItem extends Component {
     );
   }
 }
-
