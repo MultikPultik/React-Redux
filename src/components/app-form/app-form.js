5d3bb459b069661acd2a39b797690becd81359e4
AvisRara <tsiganov.da@gmail.com>
2022-01-04 21:29:15 +0400

    4.12

--- src/components/app-form/app-form.js
+++ src/components/app-form/app-form.js
@@ -15,27 +15,24 @@ export default class AppForm extends Component {
   onFormSubmit = (e) => {
     e.preventDefault();
     this.props.onAddItem(this.state.labelText);
+    this.setState({
+      labelText: ''
+    })
   };
 
   render() {
     return (
-      <form 
-        className="d-flex" 
-        onSubmit={this.onFormSubmit}
-      >
+      <form className="d-flex" onSubmit={this.onFormSubmit}>
         <input
           className="form-control app-form-add"
           type="text"
           placeholder="Что нужно сделать"
           onChange={this.onLabelChange}
+          value={this.state.labelText}
+          autoFocus
         ></input>
 
-        <button
-          className="btn btn-outline-secondary"
-          // onClick={() => {}}
-        >
-          Add
-        </button>
+        <button type="submit" className="btn btn-outline-secondary">Add</button>
       </form>
     );
   }
