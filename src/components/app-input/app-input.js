53ea39ac026bbebca6018e4423ebc3908e06f811
AvisRara <tsiganov.da@gmail.com>
2022-01-06 06:45:57 +0400

    добавил незавершенный шаблон для поиска

--- src/components/app-input/app-input.js
+++ src/components/app-input/app-input.js
@@ -2,10 +2,41 @@ import React from "react";
 
 import "./app-input.css";
 
-const AppInput = () => {
+const AppInput = ({ data, onFindData }) => {
+  // const {data} ;
+
+  const onChange = (e) => {
+    onFindData(e.target.value)
+  };
+
   return (
-    <input className="form-control search-input" placeholder="search"></input>
+    <input
+      className="form-control search-input"
+      placeholder="search"
+      onChange={onChange}
+    ></input>
   );
 };
+
+// export default class AppInput extends React.Component {
+
+//   onChange(e){
+//     const {data} = this.props;
+//     // const newData = data
+
+//     console.log(data);
+//   }
+
+//   render(){
+
+//     return (
+//       <input
+//         className="form-control search-input"
+//         placeholder="search"
+//         onChange={this.onChange}
+//       ></input>
+//     );
+//   }
+// }
 
 export default AppInput;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,10 +14,11 @@ export default class App extends Component {
 
   state = {
     appData: [
-      { label: "Drink Coffee", important: false, done:false, id: 0 },
-      { label: "Make good anyware", important: true, done:false, id: 1 },
-      { label: "Have a lunch", important: false, done:false, id: 2 },
+      { label: "Drink Coffee", important: false, done: false, id: 0 },
+      { label: "Make good anyware", important: true, done: false, id: 1 },
+      { label: "Have a lunch", important: false, done: false, id: 2 },
     ],
+    appFindData: [],
   };
 
   deleteItem = (id) => {
@@ -50,34 +51,47 @@ export default class App extends Component {
       ];
       return { appData: newArr };
     });
-  }
+  };
 
   onToggleImportant = (id) => {
-    this.toggleProp(id, 'important');
+    this.toggleProp(id, "important");
   };
 
   onToggleDone = (id) => {
-    this.toggleProp(id, 'done');
+    this.toggleProp(id, "done");
+  };
+
+  onFindData = (searchWord) => {
+    console.log(searchWord);
+    const newData = this.state.appData.filter((el) => {
+      // console.log(el.label.toLowerCase().search(e.target.value));
+      return el.label.toLowerCase().includes(searchWord.target.value);
+    });
+
+    this.setState({
+      appFindData: [...newData],
+    });
   };
 
   render() {
     const todoDone = this.state.appData.filter((el) => el.done).length;
     const todo = this.state.appData.length - todoDone;
-
+    
     return (
       <div className="container">
         <AppHeader toDo={todo} done={todoDone} />
         <div className="top-panel d-flex">
-          <AppInput />
+          <AppInput data={this.state.appData} onFindData={this.onFindData} />
           <AppItemFilters />
         </div>
         <AppList
-          data={this.state.appData}
+          data={this.state.appFindData}
+          // data={this.state.appFindData.length ? this.state.appFindData: this.state.appData}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
         />
-        <AppForm onAddItem={this.addItem}/>
+        <AppForm onAddItem={this.addItem} />
         {/* <AppAddItem onAddItem={this.addItem} /> */}
       </div>
     );
